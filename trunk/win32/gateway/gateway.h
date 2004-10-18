0d924f6257668a26e3aba793cd65a975c5961bf1
bart <bart@55c13d69-e7e2-0310-b639-a4a306287c42>
2004-10-18 22:10:34 +0000

    Windows things
    git-svn-id: svn://red.pomac.com/rage@139 55c13d69-e7e2-0310-b639-a4a306287c42

--- trunk/win32/gateway/gateway.h
+++ trunk/win32/gateway/gateway.h
@@ -1,16 +1,2 @@
 // gateway.h
-
-#pragma once
-
-using namespace System;
-
-namespace rage
-{
-	namespace gateway
-	{
-		public __gc class Main
-		{
-			// TODO: Add your methods for this class here.
-		};
-	}
-}
+// currently unused.
